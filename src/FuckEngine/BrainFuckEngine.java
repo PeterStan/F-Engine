e0f461352ab9175ef559ba49b439292b7cbe6fe1
PStanwicks <p.stanwicks@gmail.com>
2014-12-16 11:26:47 -0500

    completed run brainfuck

--- src/FuckEngine/BrainFuckEngine.java
+++ src/FuckEngine/BrainFuckEngine.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * @author Peter
@@ -17,41 +18,61 @@ public class BrainFuckEngine {
 	 * @param BrainFuck program
 	 * @return runs BrainFuck
 	 */
-	public static String runBF(int cells, String p){
-		byte[] array = new byte[cells];
-		int pointer = 0; String print = "";
-		Scanner scan = new Scanner(System.in);
+	public static String runBF(int cells, String program){
+		byte[] array = new byte[cells];	int array_pointer = 0; 
+		String print = "";
+		Stack<Integer> loop_program_position = new Stack<>(); Stack<Integer> loop_array_position = new Stack<>();
 		
-		for(int l = 0; l < p.length(); l++){ 
-			switch(p.charAt(l)){
-				case '>':
-						pointer++;
-					break;
-				case '<':
-						pointer--;
-					break;
-				case '+':
-						array[pointer]++;
-					break;
-				case '-':
-						array[pointer]--;
-					break;
-				case '.':
-						print += (char)(array[pointer]);
-					break;
-				case ',':
-						array[pointer] = (byte) scan.next().charAt(0);
-					break;
-				case '[':
-					break;
-				case ']':
-					break;
-				default:
-					break;
+		
+		for(int program_pointer = 0; program_pointer < program.length(); program_pointer++){
+			//System.out.print("("+ array_pointer + ")"+ array[array_pointer]+ "," + program.charAt(program_pointer)+ "   ");
+
+			switch(program.charAt(program_pointer)){
+			case '>':
+					array_pointer++;
+				break;
+			case '<':
+					array_pointer--;
+				break;
+			case '+':
+					array[array_pointer] += 1;
+				break;
+			case '-':
+					array[array_pointer] -= 1;
+				break;
+			case '.':
+					print += (char)(array[array_pointer]);
+				break;
+			case ',':
+					Scanner scan = new Scanner(System.in);
+					array[array_pointer] = (byte) scan.next().charAt(0);
+					scan.close();
+				break;
+			case '[':
+					loop_program_position.push(new Integer(program_pointer++));
+					loop_array_position.push(new Integer(array_pointer));
+					program_pointer--;
+									
+				break;
+			case ']':
+				if(array[loop_array_position.peek()] == 0){
+					loop_program_position.pop();
+					loop_array_position.pop();
+				}
+				else
+					program_pointer = loop_program_position.peek();
+				
+					
+					
+					
+					
+				break;
+			default:
+				break;
 			}
+			//System.out.println();
+			
 		}
-		
-		
 		
 		return print;
 	}
@@ -87,7 +108,8 @@ public class BrainFuckEngine {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("run:" + runBF(3,",."));
+		
+		System.out.println("run:" + runBF(100," ++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>."));
 		System.out.print("Done");
 
 	}
